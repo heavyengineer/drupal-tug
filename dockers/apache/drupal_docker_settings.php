6cf83a7e2c1a264449b9c0f03740828dfa8c728f
heavyengineer <steev@initsix.co.uk>
2015-04-17 00:53:33 +0100

    allow access to update.php from the users desktop browser disable notification of drupal not being able to make outbount http calls - see here https://www.drupal.org/node/588186

--- dockers/apache/drupal_docker_settings.php
+++ dockers/apache/drupal_docker_settings.php
@@ -18,6 +18,7 @@ $databases = array (
   ),
 );
 
+// settings for memcache
 if(file_exists('sites/all/modules/contrib/memcache/memcache.inc')){
 $conf['memcache_servers'] = array('memcache-server:11211'=>'default');
 $conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
@@ -28,3 +29,9 @@ $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
 $conf['page_cache_without_database'] = TRUE;
 $conf['page_cache_invoke_hooks'] = FALSE;
 }
+
+// settings to allow access to update.php from the users desktop
+$update_free_access = TRUE;
+
+// prevent the drupal http request fails from showing in the error report
+$conf['drupal_http_request_fails'] = FALSE;
