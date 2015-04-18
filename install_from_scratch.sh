c83c10b3101fa867739ce7085849a7d49ee193e0
heavyengineer <steev@initsix.co.uk>
2015-04-18 20:27:03 +0100

    copying settings into the correct dir

--- install_from_scratch.sh
+++ install_from_scratch.sh
@@ -4,6 +4,9 @@
 #######     Install from scratch
 ##############################################
 
+source ./env_variables.config
+source ./drupal_variables.config
+
 echo "installing site from scratch"
 
 # Make a backup copy of src dir if exists
@@ -18,15 +21,20 @@ fi
 echo "creating a new ./src directory"
 mkdir ./src
 
+############################################
+#####   Install Drupal
+############################################
+
 # if a version of drupal has been specified in the env_variables.conf
 if [ -n "$drupal_version" ];then
-echo "drupal_verion var has been declared as $drupal_version so we will attempt to download it"
+
+echo "drupal_version var has been declared as $drupal_version so we will attempt to download it"
 
 # Download and explode drupal
 drupal_archive=drupal-$drupal_version
 
 # if it's not already downloaded
-if [ ! -d ${drupal_archive}.tar.gz ]; then
+if [ ! -f ${drupal_archive}.tar.gz ]; then
 echo "no existing version of drupal-$drupal_version found, so we will attempt to download a new copy"
 curl --silent --output ./$drupal_archive.tar.gz  http://ftp.drupal.org/files/projects/$drupal_archive.tar.gz
 fi
@@ -45,22 +53,29 @@ mv $drupal_archive src/docroot
 
 fi
 
+###########################################
+##### Install drupal on the docker container
+##########################################
+
 # install the site from scratch
 # this command also needs the correct drupal version, else it just downloads the most recent
 # @TODO have this version concur with the drupal version in the vars so it works with xdebug
-if [ -n $drupal_version ]; then
+echo "Drupal Version == $drupal_version"
+echo "Apache Root == $apache_root"
+if [ "$drupal_version" ];then
 echo "drupal_version is set to $drupal_version so we will try and get that version"
-$vagrant_apache_docker_run  -- $drush dl --destination=$apache_root --drupal-project-rename="docroot" $drupal_archive
 fi
 
-echo "running site install on the apache-server"
-$vagrant_apache_docker_run  -- $drush_site_install
+
+#####################
+######## Install Drupal into local src directory
+#####################
 
 # if the drupal version is set we have a full version of drupal in the src directory
-# otherwise it's just the sites/* directories, which may be confusing, but you shouldn't
-# be editing them anyway... but is required for xdebug... what to do?
 # credit = http://drupal.stackexchange.com/questions/23700/how-to-find-download-latest-drupal-version-via-bash
 # get the latest version of drupal into a var
+# if no version is set then get the latest
+if [ -z "$drupal_version" ];then
 echo "getting latest version of drupal from drupal.org"
 latest_drupal_version=`wget -O- http://drupal.org/project/drupal | egrep -o 'drupal-[0-9\.]+.tar.gz' | sort -V  | tail -1`
 
@@ -78,8 +93,7 @@ mkdir -p ./src/docroot
 # explode into the local src directory
 echo "exploding $latest_drupal_version"
 tar -xzf $latest_drupal_version -C ./src/docroot --strip-components=1
-
-#move the contents of $latest_drupal_version
+fi
 
 # create useful directories 
 echo "creating featues, custom and contrib directories in ./src/docroot/sites/all/modules"
@@ -87,3 +101,17 @@ mkdir -p src/docroot/sites/all/modules/{features,custom,contrib}
 
 echo "creating libraries directory in ./src/docroot/sites/all/"
 mkdir -p src/docroot/sites/all/libraries
+
+###########################
+####### copy a working settings file
+###########################
+
+# @TODO move the settings file to somewhere obvious
+cp dockers/apache/drupal_docker_settings.php ./src/docroot/sites/default/settings.php
+
+#########################
+#### run site install on the apache-server container
+########################
+
+echo "running site install on the apache-server"
+$vagrant_apache_docker_run  -- $drush_site_install
