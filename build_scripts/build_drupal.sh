feb75bc82ea0bf7f9b819c36e9ec031f1028911a
Steve Power <steev@initsix.co.uk>
2015-05-15 01:16:17 +0100

    build from an existing site and install the correct settings.php for the containers

--- build_scripts/build_drupal.sh
+++ build_scripts/build_drupal.sh
@@ -17,6 +17,16 @@ if [ -z "$scratch" ];then
 echo "scratch is not set so we will download the repo from the repo_url"
 
 source ./build_scripts/clone_repo.sh
+source ./build_scripts/load_db.sh
+
+# if not installing from scratch then the settings file will need some manipulation
+# this needs some thinkng but easiest right now is to replace the existing settings.php
+# with one that works with the docker containers.  probably a good idea will be to rebuild
+# the settings.php - wrt the drupal hash, then this should maybe only be stored on production
+# anyway there is no need for a developer to have access to the salt
+
+source ./build_scripts/move_settings.php
+
 
 else
 
