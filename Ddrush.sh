32fb49e69302e13d48fbb61f8dd1e8c3e5208a26
Steve Power <steev@initsix.co.uk>
2015-05-15 13:35:57 +0100

    fixed missing config file location

--- Ddrush.sh
+++ Ddrush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # docker drush helper script
 # enables the user to run drush commands by supplying arguments to a container run
-source ./config/drupal_variables.config
+source ./config/vagrant_commands.config
 
 # this should accept and execute all the space seperated commands from the arguments
 # as one bug drush command.  So running ./Ddrush.sh cc all should run 'drush cc all' 
