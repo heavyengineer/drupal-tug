7d2db83218af4a0ee1847df5c54c54b29e1570fc
Steve Power <steev@initsix.co.uk>
2015-05-15 01:52:09 +0100

    modified to use new path

--- Ddrush.sh
+++ Ddrush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # docker drush helper script
 # enables the user to run drush commands by supplying arguments to a container run
-source ./drupal_variables.config
+source ./config/drupal_variables.config
 
 # this should accept and execute all the space seperated commands from the arguments
 # as one bug drush command.  So running ./Ddrush.sh cc all should run 'drush cc all' 
