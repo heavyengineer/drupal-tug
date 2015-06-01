df34c3f4964950ccbc1a52ba312b4b75830cdf88
Steve Power <steev@initsix.co.uk>
2015-06-01 02:03:21 +0100

    using the locally sourced db file instead of manually supplied and hardcoded db.sql

---
+++ build_scripts/copy_db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# the db file needs to be made available to the vagrant and docker instances
+# because nfs can't traverse links. 
+# check the file hasn't already been copied (md5sum?)
+# copy it to the db/ directory if it hasnt
+# previously we were using a static filename, db.sql.  this is no longer needed as we can reference the 
+# name from the .config file. might be some use to version these files tho?
+
+. config/env_variables.config
+
+if [ ! -d "db" ];then
+	echo "db directory missing, recreating"
+  mkdir db
+fi
+
+# check if the file has already been copied 
+filename=${sql_url##*/}
+
+if [ ! -f "db/${filename}" ];then 
+echo "copying ${filename}"
+  cp ${sql_url} ./db
+  else
+	  echo "${filename} already exists"
+fi
+
+
+
--- load_sql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# load the sql from a file because of the way docker escapes shell chars
-
-#/usr/bin/drush -y sql-cli --db-url=mysql://admin:changeme@mysql-server/drupal < /vagrant/db/db.sql
-/root/.composer/vendor/bin/drush -y sql-cli --db-url=mysql://admin:changeme@mysql-server/drupal < /vagrant/db/db.sql
