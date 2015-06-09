6bc3cf2a5a97943aa9477e073c5d1992e724f98e
steev <steev@initsix.co.uk>
2015-06-09 17:45:23 +0100

    updated to use new location for vagrant commands file

--- backupdb.sh
+++ backupdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # backup the current database and place into the db directory with a timestamp set
-source ./drupal_variables.config
+source ./config/vagrant_commands.config
 
 date=`date +%s`
 $vagrant_apache_docker_run -- $drush sql-dump --result-file=/vagrant/db/bak.${date}.sql
