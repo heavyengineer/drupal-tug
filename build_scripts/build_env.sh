64de2acdf49a386fa81b4a54162f5e697156236d
steev <steev@initsix.co.uk>
2015-06-30 18:14:53 +0100

    updates while vagrant is crashing all around me 8(

--- build_scripts/build_env.sh
+++ build_scripts/build_env.sh
@@ -8,7 +8,7 @@ vagrant up --provider=docker --no-parallel
 echo "Build local development environment"
 
 # source the db file to make it available to vagrant + Docker
-source copy_db.sh
+#source copy_db.sh
 
 # do drupal build here
-./build_scripts/build_drupal.sh
+#./build_scripts/build_drupal.sh
